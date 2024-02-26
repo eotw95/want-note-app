d533a556460bb0cf64a68af43792664359bc53f2
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-02-27 00:23:37 +0900

    Add process that display inserted items

--- app/src/main/java/com/eotw95/wantnote/screen/WantList.kt
+++ app/src/main/java/com/eotw95/wantnote/screen/WantList.kt
@@ -1,21 +1,23 @@
 package com.eotw95.wantnote.screen
 
 import android.app.Application
+import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.eotw95.wantnote.R
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun WantList(
@@ -31,17 +33,21 @@ fun WantList(
     }
 
     val items = viewModel?.items?.observeAsState()
-
-    // Todo: fetch dummy data
     viewModel?.fetch()
     println("WantList items=$items")
-
 
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 150.dp),
         content = {
-            items(30) {
-                GridItem(onClickItem)
+            val list = items?.value
+            if (!list.isNullOrEmpty()) {
+                items(list) { item ->
+                    Log.d("WantList", "item=$item")
+                    GridItem(
+                        imageUrl = item.imageUri,
+                        onClickItem = onClickItem
+                    )
+                }
             }
         }
     )
@@ -49,15 +55,17 @@ fun WantList(
 
 @Composable
 fun GridItem(
+    imageUrl: String,
     onClickItem: () -> Unit
 ) {
+    Log.d("GridItem", "imageUri=${Uri.parse(imageUrl)}")
     Surface(
         modifier = Modifier
             .padding(1.dp)
             .clickable { onClickItem() }
     ) {
         Image(
-            painter = painterResource(id = R.drawable.sample_image),
+            painter = rememberAsyncImagePainter(model = Uri.parse(imageUrl)),
             contentDescription = null
         )
     }
