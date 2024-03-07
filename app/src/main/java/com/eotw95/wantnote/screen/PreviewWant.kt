cbb779e27a609bd2cf83cb591b963e97567140ad
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-03-07 23:03:12 +0900

    Fix image aspect ratio

--- app/src/main/java/com/eotw95/wantnote/screen/PreviewWant.kt
+++ app/src/main/java/com/eotw95/wantnote/screen/PreviewWant.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -22,7 +23,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -99,7 +99,7 @@ fun PreviewWant(
                 Image(
                     painter = rememberAsyncImagePainter(model = File(imagePath)),
                     contentDescription = null,
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier.fillMaxWidth().aspectRatio(1f)
                 )
                 Spacer(modifier = Modifier.padding(vertical = 30.dp))
                 Row(
