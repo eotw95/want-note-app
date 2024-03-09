65c1265fc31ea8ca9bec85512feabf56e363ee01
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-03-09 17:21:37 +0900

    Add enable JavaScript

--- app/src/main/java/com/eotw95/wantnote/screen/WebWant.kt
+++ app/src/main/java/com/eotw95/wantnote/screen/WebWant.kt
@@ -1,20 +1,24 @@
 package com.eotw95.wantnote.screen
 
+import android.annotation.SuppressLint
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.viewinterop.AndroidView
 
+@SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun WebWant(url: String) {
     AndroidView(factory = { context ->
         WebView(context).apply {
             webViewClient = WebViewClient()
-            // Permission access mixed contents HTTP and HTTPS
-            settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-            // not load cache
-            settings.cacheMode = WebSettings.LOAD_NO_CACHE
+            settings.apply {
+                // Permission access mixed contents HTTP and HTTPS
+                mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+                // JavaScript enable
+                javaScriptEnabled = true
+            }
             loadUrl(url)
         }
     }
