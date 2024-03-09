19c52abf4417bb8b13ab7b649b96927363c758c7
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-03-09 10:58:03 +0900

    Add permission connect to HTTP
    not load cache

--- app/src/main/java/com/eotw95/wantnote/screen/WebWant.kt
+++ app/src/main/java/com/eotw95/wantnote/screen/WebWant.kt
@@ -1,5 +1,6 @@
 package com.eotw95.wantnote.screen
 
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
@@ -10,6 +11,10 @@ fun WebWant(url: String) {
     AndroidView(factory = { context ->
         WebView(context).apply {
             webViewClient = WebViewClient()
+            // Permission access mixed contents HTTP and HTTPS
+            settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            // not load cache
+            settings.cacheMode = WebSettings.LOAD_NO_CACHE
             loadUrl(url)
         }
     }
