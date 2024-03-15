b47c5dfa59e916444c91be169167a8f9d9f765b1
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-03-15 22:13:05 +0900

    Fix UI

--- app/src/main/java/com/eotw95/wantnote/screen/PreviewWant.kt
+++ app/src/main/java/com/eotw95/wantnote/screen/PreviewWant.kt
@@ -4,18 +4,20 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -24,6 +26,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
@@ -59,17 +63,22 @@ fun PreviewWant(
     Scaffold(
         topBar = {
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Black)
+                    .padding(horizontal = 10.dp, vertical = 10.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Icon(
                     imageVector = Icons.Filled.ArrowBack,
                     contentDescription = null,
+                    tint = Color.White,
                     modifier = Modifier.clickable { onClickBack() }
                 )
                 Icon(
                     imageVector = Icons.Filled.Delete,
                     contentDescription = null,
+                    tint = Color.White,
                     modifier = Modifier.clickable {
                         Log.d("PreviewWantList", "id=$id")
                         previewVM?.delete(id)
@@ -84,18 +93,18 @@ fun PreviewWant(
                 .padding(it)
         ) {
             Column(
-                modifier = Modifier
-                    .fillMaxWidth(),
-//                Todo: スクロールしたいけどこれだと画像が表示されない
-//                    .verticalScroll(rememberScrollState(),
-                horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier.fillMaxWidth().background(Color.Black),
+                horizontalAlignment = Alignment.Start
             ) {
                 Image(
                     painter = rememberAsyncImagePainter(model = File(imagePath)),
                     contentDescription = null,
-                    modifier = Modifier.fillMaxWidth().aspectRatio(1f)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp),
+                    contentScale = ContentScale.FillWidth
                 )
-                Spacer(modifier = Modifier.padding(vertical = 30.dp))
+                Spacer(modifier = Modifier.padding(vertical = 15.dp))
 
                 var isLink = false
                 val linkText = if (link.startsWith("http")) {
@@ -109,16 +118,24 @@ fun PreviewWant(
 
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.clickable {
+                    modifier = Modifier
+                        .padding(horizontal = 20.dp)
+                        .clickable {
                         if (isLink) onClickUri(encoderUrl)
                     }
                 ) {
-                    Icon(imageVector = Icons.Filled.ExitToApp, contentDescription = null)
+                    Icon(imageVector = Icons.Filled.ExitToApp, contentDescription = null, tint = Color.White)
                     Spacer(modifier = Modifier.padding(horizontal = 5.dp))
-                    Text(text = linkText)
+                    Text(text = linkText, color = Color.White)
                 }
-                Spacer(modifier = Modifier.padding(vertical = 30.dp))
-                Text(text = if (description == KEY_EMPTY) "" else description)
+                Spacer(modifier = Modifier.padding(vertical = 15.dp))
+                Divider(color = Color.White, thickness = 0.2.dp, modifier = Modifier.padding(horizontal = 15.dp))
+                Spacer(modifier = Modifier.padding(vertical = 15.dp))
+                Text(
+                    text = if (description == KEY_EMPTY) "" else description,
+                    modifier = Modifier.padding(horizontal = 15.dp),
+                    color = Color.White
+                )
             }
         }
     }
